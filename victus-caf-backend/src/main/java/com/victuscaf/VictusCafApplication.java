dfac7bbdb2838d407276c4cf2eb15cf49b18cd3e
santiruizi <ruizgallegosantiago896@gmail.com>
2026-05-29 00:13:11 -0500

    Primera estructuracion de carpetas sobre el proyecto

---
+++ victus-caf-backend/src/main/java/com/victuscaf/VictusCafApplication.java
@@ -0,0 +1,13 @@
+package com.victuscaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictusCafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VictusCafApplication.class, args);
+    }
+
+}
